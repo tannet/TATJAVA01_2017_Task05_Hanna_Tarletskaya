ef0a7776128893fcc4b312f38570045d1c8f74d2
Hanna Tarletskaya <anna9zorina@mail.ru>
2017-03-03 08:28:07 +0300

    Update MenuTagName.java

--- src/com/epam/xml/entity/MenuTagName.java
+++ src/com/epam/xml/entity/MenuTagName.java
@@ -1,26 +1,23 @@
 package com.epam.xml.entity;
-
 public enum MenuTagName {
-	NAME, DESCRIPTION, WEIGHT, PRICE, FOOD, BREAKFAST_MENU;
+	NAME, PRICE, DESCRIPTION, WEIGHT, FOOD, CATEGORY, MENU;
 
-	// for stax only?
 	public static MenuTagName getElementTagName(String element) {
 		switch (element) {
-		case "name":
-			return NAME;
+		case "menu":
+			return MENU;
+		case "food":
+			return FOOD;
+		case "price":
+			return PRICE;
 		case "description":
 			return DESCRIPTION;
 		case "weight":
 			return WEIGHT;
-		case "price":
-			return PRICE;
-
-		case "food":
-			return FOOD;
-
-		case "breakfast-menu":
-			return BREAKFAST_MENU;
-
+		case "category":
+			return CATEGORY;
+		case "name":
+			return NAME;
 		default:
 			throw new EnumConstantNotPresentException(MenuTagName.class, element);
 		}
