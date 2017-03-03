560178faaf347d938ee490a7b2af6b7ab92d64da
Hanna Tarletskaya <anna9zorina@mail.ru>
2017-03-03 08:30:17 +0300

    Update SAXMenuParser.java

--- src/com/epam/xml/parser/SAXMenuParser.java
+++ src/com/epam/xml/parser/SAXMenuParser.java
@@ -1,5 +1,28 @@
 package com.epam.xml.parser;
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import com.epam.xml.entity.Food;
 
 public class SAXMenuParser {
-
+	public static void parseSAX() throws ParserConfigurationException, SAXException, IOException {
+		System.out.println("SAX");
+		XMLReader reader = XMLReaderFactory.createXMLReader();
+		MenuSaxHandler handler = new MenuSaxHandler();
+		reader.setContentHandler(handler);
+		reader.parse(new InputSource("menu.xml"));
+		reader.setFeature("http://xml.org/sax/features/validation", true);
+		reader.setFeature("http://xml.org/sax/features/namespaces", true);
+		reader.setFeature("http://xml.org/sax/features/string-interning", true);
+		reader.setFeature("http://apache.org/xml/features/validation/schema", false);
+		List<Food> menu = handler.getFoodList();
+		for (Food food : menu) {
+			System.out.println(food.toString());
+		}
+	}
 }
