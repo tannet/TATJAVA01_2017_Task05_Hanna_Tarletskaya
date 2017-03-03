8b106c5b766ea27227e7845b9f75f5c3556f35ad
Hanna Tarletskaya <anna9zorina@mail.ru>
2017-03-03 08:32:52 +0300

    Update Runner.java

--- src/com/epam/xml/view/Runner.java
+++ src/com/epam/xml/view/Runner.java
@@ -1,10 +1,28 @@
 package com.epam.xml.view;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import com.epam.xml.factory.BuilderFactory;
 
 public class Runner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		Helper.printHelp();// write it!
+		Scanner sc = new Scanner(System.in);
+		String command;
+		while (sc.hasNext()) {
+			command = sc.nextLine();
+			if (command.equals("EXIT")) {
+				System.out.println("Program executing is stopped.");
+				return;
+			} else {
+				BuilderFactory factory = new BuilderFactory();
+				factory.createFoodBuilder(command);
+			}
+		}
 	}
-
 }
