2bb04453efc7e57eb1f853f4eec848632a6c20e7
Hanna Tarletskaya <anna9zorina@mail.ru>
2017-03-03 08:27:31 +0300

    Update Food.java

--- src/com/epam/xml/entity/Food.java
+++ src/com/epam/xml/entity/Food.java
@@ -2,56 +2,72 @@ package com.epam.xml.entity;
 
 public class Food {
 	private int id;
-	private String type;
+	private String category;
 	private String name;
+	private String price;
 	private String description;
-	private String weight; //(?)
-	private int price;
-	
-	public Food(){
-		
-	}
-	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getWeight() {
-		return weight;
-	}
-	public void setWeight(String weight) {
-		this.weight = weight;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
+	private String weight;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String string) {
+		this.weight = string;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
 	@Override
 	public String toString() {
-		return "Food [id=" + id + ", type=" + type + ", name=" + name + ", description=" + description + ", weight="
-				+ weight + ", price=" + price + "]";
-	}	
+		if (category.equals("")) {
+			setDescription("?");
+		}
+		if (price.equals("")) {
+			setPrice("?");
+		}
+		if (description.equals("")) {
+			setDescription("?");
+		}
+		return "Food [id=" + id + ". Category: " + category + ". Name: " + name + ". Description: " + description + ", weight=" + "" + weight
+				+ ", price=" + price + "]";
+	}
 }
